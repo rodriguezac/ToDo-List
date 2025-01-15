bf3f568af15783a794d93941e6b0cbbd592b05bc
Angélica Rodríguez <rodriguezq.angelicac@gmail.com>
2025-01-14 22:10:05 -0500

    Cambio de nombre de icono

--- src/components/ToDo_Items.jsx
+++ src/components/ToDo_Items.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import done from '../assets/done.png'
 import undone from '../assets/undone.png'
-import borrar from '../assets/borrar.png'
+import trash from '../assets/trash.png'
 
 const ToDo_Items= ({text, isComplete, id,deleteToDo, toggle})=>{
   return (
@@ -9,12 +9,12 @@ const ToDo_Items= ({text, isComplete, id,deleteToDo, toggle})=>{
 
       <section onClick={()=> {toggle (id)}} className='flex flex-1 items-center cursor-pointer' >
 
-        <img className='w-7 sm: max-w-5' src={isComplete ? done: undone} alt="Tarea hecha icon " />
+        <img className='w-7 sm: max-w-5' src={isComplete ? done: undone} alt="Tarea hecha - icon " />
         <p className={`text-slate-700 ml-4 text-[17px] sm: text-xs ${isComplete ?"line-through":""}`}> {text}</p>
 
       </section>
 
-      <img onClick={()=>{deleteToDo(id)}} className='w-3.5 cursor-pointer' src={borrar} alt="borra la tare -icon" />
+      <img onClick={()=>{deleteToDo(id)}} className='w-3.5 cursor-pointer' src={trash} alt="borra la tarea -icon" />
     </div>
   )
 }
